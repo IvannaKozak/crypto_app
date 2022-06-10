8a3b26ea0f0b0f89558a7a9e4d06932bbfcfb755
IvannaKozak <ivanka.kozak3@gmail.com>
2022-06-10 16:22:15 +0300

    commit

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,24 +1,34 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:crypto_app/price_screen.dart';
 
 const apiKey = 'CC9EF9B5-E83D-4B07-849A-D7B88D784254';
 const coinApiURL = 'https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=$apiKey';
 
 
 class Currency {
-  Future<dynamic> getRate() async {
-   
-    http.Response response = await http.get(Uri.parse(coinApiURL));
-    if (response.statusCode == 200) {
-    
-      /// interprets a given string as JSON
-      var decodedData = jsonDecode(response.body);
-      var rate = decodedData['rate'];
-      return rate;
-    } else {
-      print(response.statusCode);
-      throw 'Problem with the get request';
+  Future<dynamic> getRate(String currencySelected) async {
+    Map<String, String> cryptoPrices = {};
+
+    for (String crypto in cryptoList) {
+      String url =
+        'https://rest.coinapi.io/v1/exchangerate/$crypto/$currencySelected?apikey=$apiKey';
+
+      http.Response response = await http.get(Uri.parse(url));
+
+      
+      if (response.statusCode == 200) {
+      
+        /// interprets a given string as JSON
+        var decodedData = jsonDecode(response.body);
+        double rate = decodedData['rate'];
+        cryptoPrices[crypto] = rate.toStringAsFixed(0);
+      } else {
+        print(response.statusCode);
+        throw 'Problem with the get request';
+      }
     }
+    return cryptoPrices;
   }
 }
 
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:crypto_app/coin_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'dart:io' show Platform;
+import 'package:crypto_app/cardbutton.dart';
 
 
 
@@ -12,28 +13,66 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = 'USD';
+  Map<String, String> coinValues = {};
+  bool isWaiting = false;
+  String value;
 
-  DropdownButton<String> androidDropdown() {
-    List<DropdownMenuItem<String>> dropdownItems = [];
+  @override
+  void initState() {
+    super.initState();
+    getCurrenciesData();
+  }
+
+  getCurrenciesData() async {
+    isWaiting = true;
+    try {
+      dynamic data = await Currency().getRate(selectedCurrency);
+      isWaiting = false;
+      setState(() {
+        coinValues = data;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // DropdownButton<String> androidDropdown() {
+  //   List<DropdownMenuItem<String>> dropdownItems = [];
+  //   for (String currency in currenciesList) {
+  //     var newItem = DropdownMenuItem(
+  //       child: Text(currency),
+  //       value: currency,
+  //     );
+  //     dropdownItems.add(newItem);
+  //   }
+  //   return DropdownButton<String>(
+  //             value: selectedCurrency,
+  //             items: dropdownItems, 
+  //             onChanged: (value) {
+  //               setState(() {
+  //                 selectedCurrency = value;
+  //                 getCurrenciesData();
+  //                 print(selectedCurrency);
+  //               },
+  //               );
+  //             },
+  //           );
+  //   }
+  
+  List<DropdownMenuItem> dropDown() {
+    List<DropdownMenuItem<String>> dropDownItems = [];
+
     for (String currency in currenciesList) {
+      //for every currency in the list we create a new dropdownmenu item
       var newItem = DropdownMenuItem(
         child: Text(currency),
         value: currency,
       );
-      dropdownItems.add(newItem);
+      // add to the list of menu item
+      dropDownItems.add(newItem);
     }
-    return DropdownButton<String>(
-              value: selectedCurrency,
-              items: dropdownItems, 
-              onChanged: (value) {
-                setState(() {
-                  selectedCurrency = value;
-                  print(selectedCurrency);
-                },
-                );
-              },
-            );
-    }
+    return dropDownItems;
+  }
 
   CupertinoPicker iOSPicker() {
     List<Text> pickerItems = [];
@@ -53,27 +92,8 @@ class _PriceScreenState extends State<PriceScreen> {
     );
   }
 
-  bool isWaiting = false;
   
-  String value;
-  @override
-  void initState() {
-    super.initState();
-    getCurrenciesData();
-  }
 
-  getCurrenciesData() async {
-    isWaiting = true;
-    try {
-      double data = await Currency().getRate();
-      isWaiting = false;
-      setState(() {
-        value = data.toStringAsFixed(0);
-      });
-    } catch (e) {
-      print(e);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -86,33 +106,56 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = $value USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
+          CardButton(cryptoCurrency: 'BTC',
+                  selectedCurrency: selectedCurrency,
+                  /// this is a ternary opertor, it checks if is waiting is true, it displays a ? but if its false
+                  /// meaning all operations are done  it prints the value of BTC(key) in the coinValues map.
+                  value: isWaiting ? '?' : coinValues['BTC'],),
+          CardButton(cryptoCurrency: 'ETH',
+                  selectedCurrency: selectedCurrency,
+                  
+                  value: isWaiting ? '?' : coinValues['ETH'],),
+          CardButton(cryptoCurrency: 'LTC',
+                  selectedCurrency: selectedCurrency,
+                  value: isWaiting ? '?' : coinValues['LTC'],),
+          // Padding(
+          //   padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+          //   child: Card(
+          //     color: Colors.lightBlueAccent,
+          //     elevation: 5.0,
+          //     shape: RoundedRectangleBorder(
+          //       borderRadius: BorderRadius.circular(10.0),
+          //     ),
+          //     child: Padding(
+          //       padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+          //       child: Text(
+          //         '1 BTC = $value USD',
+          //         textAlign: TextAlign.center,
+          //         style: TextStyle(
+          //           fontSize: 20.0,
+          //           color: Colors.white,
+          //         ),
+          //       ),
+          //     ),
+          //   ),
+          // ),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: iOSPicker(),  //Platform.isIOS ? iOSPicker() : androidDropdown(),
+            child: DropdownButton<String>(
+              dropdownColor: Colors.lightBlue,
+              value: selectedCurrency,
+              items: dropDown(),
+              onChanged: (value) {
+                setState(() {
+                  selectedCurrency = value;
+                  /// call this function each time the value in the drop down changes.
+                  getCurrenciesData();
+                });
+              },
+            ),//Platform.isIOS ? iOSPicker() : androidDropdown(),
           ),
         ],
       ),
